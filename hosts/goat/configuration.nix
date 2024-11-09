8c1d0b2a979341c3c6dbb423e65efc1bfd77e676
Chucklee1 <91444983+Chucklee1@users.noreply.github.com>
2024-11-08 22:10:10 -0800

    Update configuration.nix

--- hosts/goat/configuration.nix
+++ hosts/goat/configuration.nix
@@ -3,30 +3,20 @@
   config,
   ...
 }:
-# desktop conf
 {
   imports = [
     ./hardware-configuration.nix
     ../../modules/default.nix
   ];
-  # modules
+
+  # modules 
   nvidia.enable = true;
-  niri.enable = true;
-  X11.enable = true;
-
-  # hostname
-  networking.hostName = "goat"; 
-
-  # user account
-  users.users.gaot = {
+  
+  # user
+  networking.hostName = "goat";
+  users.users.goat = {
     isNormalUser = true;
     description = "goat";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-  
-
-  # ================================================================ #
-  # =                         DO NOT TOUCH                         = #
-  # ================================================================ #
-  system.stateVersion = "24.05"; # D O  N O T  C H A N G E
-}
+    extraGroups = ["networkmanager" "wheel"];
+  }; 
+ } 
